6844e0c8a83106514989ba99cb691cf8c3b616d4
Brian Kirkpatrick <code@tythos.net>
2019-06-11 13:00:48 -0700

    Prepping for transcription of build script (no longer ignored)

---
+++ build/setup.py
@@ -0,0 +1,15 @@
+"""Forwards call to setuptools.setup() after parsing:
+   * name from package folder
+   * long description from README.rst
+   * packages from setuptools.find_packages()
+   * remaining properties from package-level package.json
+"""
+
+def main():
+    """
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
+
